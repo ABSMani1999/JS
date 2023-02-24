e42b9a68d03afda8157bc894e725f4b09610841f
Balasubramanian1999 <absmani1999@gmail.com>
2023-02-24 07:01:08 +0530

    new file add

---
+++ First class/index.js
@@ -0,0 +1,82 @@
+//var
+var x=5;
+var y=10;
+var x=22;
+console.log(x+y);
+
+document.write(x+y);
+
+//let
+
+let a=8;
+let b=12;
+console.log(a+b);
+
+document.write(a+b);
+
+//const
+const price1=5;
+
+const price=13;
+console.log (price1+price);
+
+document.write (price1+price);
+
+//var+let-const
+
+var p=12;
+let q=8;
+const r=9;
+
+console.log (p+q+r);
+console.log (typeof p+q+r);
+
+
+
+
+//nothing
+x=5;
+y=12;
+console.log(x+y)
+
+document.write(x-y)
+
+//string
+var x='12';
+var y=8;
+
+console.log(x+y);
+
+//Number
+ let num1=3;
+ let num2=7;
+ let num3=5;
+ 
+ console.log(num1+num2-num3);
+ 
+ 
+ //boolean
+ 
+ console.log(18>24)
+ 
+ 
+ //undefined
+ 
+ let name;
+ 
+ console.log(name);
+ 
+ //null
+ 
+ 
+ 
+
+ 
+
+
+
+
+
+
+
+
