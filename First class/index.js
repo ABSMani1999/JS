65ce3f38d12e8787b9b9a9422ec90b989c9c5d17
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-08 15:35:26 +0530

    update

--- First class/index.js
+++ First class/index.js
@@ -1,26 +1,27 @@
+//variable type
 //var
 var x=5;
 var y=10;
 var x=22;
-console.log(x+y);
 
 document.write(x+y);
+document.write("<br>");
 
 //let
 
 let a=8;
 let b=12;
-console.log(a+b);
 
 document.write(a+b);
+document.write("<br>");
 
 //const
-const price1=5;
 
+const price1=5;
 const price=13;
-console.log (price1+price);
 
 document.write (price1+price);
+document.write("<br>");
 
 //var+let-const
 
@@ -28,43 +29,46 @@ var p=12;
 let q=8;
 const r=9;
 
-console.log (p+q+r);
-console.log (typeof p+q+r);
-
-
-
+document.write(p+q+r);
+document.write("<br>");
+document.write(typeof p+q+r);
+document.write("<br>");
 
 //nothing
+
 x=5;
 y=12;
-console.log(x+y)
 
-document.write(x-y)
+document.write(x-y);
+document.write("<br>");
 
+//data type
 //string
 var x='12';
 var y=8;
 
-console.log(x+y);
+document.write(x+y);
+document.write("<br>");
 
 //Number
  let num1=3;
  let num2=7;
  let num3=5;
  
- console.log(num1+num2-num3);
- 
+document.write(num1+num2-num3);
+document.write("<br>");
  
  //boolean
  
- console.log(18>24)
- 
+ document.write(18>24);
+ document.write("<br>");
  
  //undefined
  
  let name;
  
- console.log(name);
+ document.write(name);
+ document.write("<br>");
  
  //null
  
